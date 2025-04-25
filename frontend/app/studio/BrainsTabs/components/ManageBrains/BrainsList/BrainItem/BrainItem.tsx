ca7dd3f5612b8197a74f3b30980df05845f13378
thienduyy <nguyenvthienduy@gmail.com>
2025-04-25 16:29:03 +0700

    feat(llm): update LLM model and configuration parameters
    - Changed default LLM model from "gpt-3.5-turbo-0125" to "gpt-40-mini".
    - Increased max_input and max_output tokens to 10000 for improved context handling.
    - Updated CreateBrainProperties to use "gpt-4o-mini" as the default model and increased max_tokens to 4000.
    - Adjusted default model in configuration to "gpt-4o-mini" for consistency across the application.

--- frontend/app/studio/BrainsTabs/components/ManageBrains/BrainsList/BrainItem/BrainItem.tsx
+++ frontend/app/studio/BrainsTabs/components/ManageBrains/BrainsList/BrainItem/BrainItem.tsx
@@ -16,147 +16,150 @@ import { Option } from "@/lib/types/Options";
 import styles from "./BrainItem.module.scss";
 
 type BrainItemProps = {
-	brain: MinimalBrainForUser;
-	even: boolean;
+  brain: MinimalBrainForUser;
+  even: boolean;
 };
 
 export const BrainItem = ({ brain, even }: BrainItemProps): JSX.Element => {
-	const { t } = useTranslation(["translation", "brain"]);
+  const { t } = useTranslation(["translation", "brain"]);
 
-	const [optionsOpened, setOptionsOpened] = useState<boolean>(false);
-	const [optionsHovered, setOptionsHovered] = useState<boolean>(false);
+  const [optionsOpened, setOptionsOpened] = useState<boolean>(false);
+  const [optionsHovered, setOptionsHovered] = useState<boolean>(false);
 
-	const {
-		handleUnsubscribeOrDeleteBrain,
-		isDeleteOrUnsubscribeModalOpened,
-		setIsDeleteOrUnsubscribeModalOpened,
-		isDeleteOrUnsubscribeRequestPending,
-	} = useBrainManagementTabs(brain.id);
-	const { allBrains, setCurrentBrainId } = useBrainContext();
-	const { isOwnedByCurrentUser } = getBrainPermissions({
-		brainId: brain.id,
-		userAccessibleBrains: allBrains,
-	});
-	const { brain: fetchedBrain } = useBrainFetcher({ brainId: brain.id });
-	const { isDarkMode } = useUserSettingsContext();
-	const { setIsVisible } = useSearchModalContext();
+  const {
+    handleUnsubscribeOrDeleteBrain,
+    isDeleteOrUnsubscribeModalOpened,
+    setIsDeleteOrUnsubscribeModalOpened,
+    isDeleteOrUnsubscribeRequestPending,
+  } = useBrainManagementTabs(brain.id);
+  const { allBrains, setCurrentBrainId } = useBrainContext();
+  const { isOwnedByCurrentUser } = getBrainPermissions({
+    brainId: brain.id,
+    userAccessibleBrains: allBrains,
+  });
+  const { brain: fetchedBrain } = useBrainFetcher({ brainId: brain.id });
+  const { isDarkMode } = useUserSettingsContext();
+  const { setIsVisible } = useSearchModalContext();
 
-	const iconRef = useRef<HTMLDivElement | null>(null);
-	const optionsRef = useRef<HTMLDivElement | null>(null);
+  const iconRef = useRef<HTMLDivElement | null>(null);
+  const optionsRef = useRef<HTMLDivElement | null>(null);
 
-	const options: Option[] = [
-		{
-			label: t("editButton", { ns: "brain" }),
-			onClick: () => (window.location.href = `/studio/${brain.id}`),
-			iconName: "edit",
-			iconColor: "primary",
-		},
-		{
-			label: t("talkButton", { ns: "brain" }),
-			onClick: () => {
-				setOptionsOpened(false);
-				setIsVisible(true);
-				setTimeout(() => setCurrentBrainId(brain.id));
-			},
-			iconName: "chat",
-			iconColor: "primary",
-		},
-		{
-			label: t("deleteButton", { ns: "brain" }),
-			onClick: () => void setIsDeleteOrUnsubscribeModalOpened(true),
-			iconName: "delete",
-			iconColor: "dangerous",
-		},
-	];
+  const options: Option[] = [
+    {
+      label: t("editButton", { ns: "brain" }),
+      onClick: () => (window.location.href = `/studio/${brain.id}`),
+      iconName: "edit",
+      iconColor: "primary",
+    },
+    {
+      label: t("talkButton", { ns: "brain" }),
+      onClick: () => {
+        setOptionsOpened(false);
+        setIsVisible(true);
+        setTimeout(() => setCurrentBrainId(brain.id));
+      },
+      iconName: "chat",
+      iconColor: "primary",
+    },
+    {
+      label: t("deleteButton", { ns: "brain" }),
+      onClick: () => void setIsDeleteOrUnsubscribeModalOpened(true),
+      iconName: "delete",
+      iconColor: "dangerous",
+    },
+  ];
 
-	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (
-				iconRef.current &&
-				!iconRef.current.contains(event.target as Node) &&
-				optionsRef.current &&
-				!optionsRef.current.contains(event.target as Node)
-			) {
-				setOptionsOpened(false);
-			}
-		};
-		document.addEventListener("mousedown", handleClickOutside);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        iconRef.current &&
+        !iconRef.current.contains(event.target as Node) &&
+        optionsRef.current &&
+        !optionsRef.current.contains(event.target as Node)
+      ) {
+        setOptionsOpened(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
 
-		return () => {
-			document.removeEventListener("mousedown", handleClickOutside);
-		};
-	}, [isDarkMode]);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isDarkMode]);
 
-	return (
-		<>
-			<a
-				className={`
+  return (
+    <>
+      <a
+        className={`
       ${even ? styles.even : styles.odd}
       ${styles.brain_item_wrapper} ${optionsHovered ? styles.not_hovered : ""}
       `}
-				href={`/studio/${brain.id}`}
-			>
-				<div className={styles.brain_header}>
-					<div className={styles.left}>
-						<div
-							className={styles.brain_snippet}
-							style={{ backgroundColor: brain.snippet_color }}
-						>
-							<span>{brain.snippet_emoji}</span>
-						</div>
-						<span className={styles.name}>{brain.name}</span>
-					</div>
-					<div
-						ref={iconRef}
-						onClick={(event: React.MouseEvent<HTMLElement>) => {
-							event.nativeEvent.stopImmediatePropagation();
-							event.stopPropagation();
-							event.preventDefault();
-							setOptionsOpened(!optionsOpened);
-						}}
-						className={styles.icon}
-					>
-						<div
-							onMouseEnter={() => setOptionsHovered(true)}
-							onMouseLeave={() => setOptionsHovered(false)}
-						>
-							<Icon
-								name="options"
-								size="small"
-								color="black"
-								handleHover={true}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className={styles.model}>
-					<span className={styles.title}>{t("models", { ns: "translation" })}{":"}</span>
-					<span className={styles.model_type}>
-						{fetchedBrain?.model ?? "gpt-3.5-turbo-0125"}
-					</span>
-				</div>
+        href={`/studio/${brain.id}`}
+      >
+        <div className={styles.brain_header}>
+          <div className={styles.left}>
+            <div
+              className={styles.brain_snippet}
+              style={{ backgroundColor: brain.snippet_color }}
+            >
+              <span>{brain.snippet_emoji}</span>
+            </div>
+            <span className={styles.name}>{brain.name}</span>
+          </div>
+          <div
+            ref={iconRef}
+            onClick={(event: React.MouseEvent<HTMLElement>) => {
+              event.nativeEvent.stopImmediatePropagation();
+              event.stopPropagation();
+              event.preventDefault();
+              setOptionsOpened(!optionsOpened);
+            }}
+            className={styles.icon}
+          >
+            <div
+              onMouseEnter={() => setOptionsHovered(true)}
+              onMouseLeave={() => setOptionsHovered(false)}
+            >
+              <Icon
+                name="options"
+                size="small"
+                color="black"
+                handleHover={true}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.model}>
+          <span className={styles.title}>
+            {t("models", { ns: "translation" })}
+            {":"}
+          </span>
+          <span className={styles.model_type}>
+            {fetchedBrain?.model ?? "gpt-4o-mini"}
+          </span>
+        </div>
 
-				<span className={styles.description}>{brain.description}</span>
-				<div>
-					<DeleteOrUnsubscribeConfirmationModal
-						isOpen={isDeleteOrUnsubscribeModalOpened}
-						setOpen={setIsDeleteOrUnsubscribeModalOpened}
-						onConfirm={() => void handleUnsubscribeOrDeleteBrain()}
-						isOwnedByCurrentUser={isOwnedByCurrentUser}
-						isDeleteOrUnsubscribeRequestPending={
-							isDeleteOrUnsubscribeRequestPending
-						}
-					/>
-				</div>
-				<div
-					ref={optionsRef}
-					className={styles.options_modal}
-					onMouseEnter={() => setOptionsHovered(true)}
-					onMouseLeave={() => setOptionsHovered(false)}
-				>
-					{optionsOpened && <OptionsModal options={options} />}
-				</div>
-			</a>
-		</>
-	);
+        <span className={styles.description}>{brain.description}</span>
+        <div>
+          <DeleteOrUnsubscribeConfirmationModal
+            isOpen={isDeleteOrUnsubscribeModalOpened}
+            setOpen={setIsDeleteOrUnsubscribeModalOpened}
+            onConfirm={() => void handleUnsubscribeOrDeleteBrain()}
+            isOwnedByCurrentUser={isOwnedByCurrentUser}
+            isDeleteOrUnsubscribeRequestPending={
+              isDeleteOrUnsubscribeRequestPending
+            }
+          />
+        </div>
+        <div
+          ref={optionsRef}
+          className={styles.options_modal}
+          onMouseEnter={() => setOptionsHovered(true)}
+          onMouseLeave={() => setOptionsHovered(false)}
+        >
+          {optionsOpened && <OptionsModal options={options} />}
+        </div>
+      </a>
+    </>
+  );
 };
